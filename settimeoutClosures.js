105168d7d1c0c80d4c892dc3acbe821c79a5de2c
zeenathkousar <95568153+zeenathkousar@users.noreply.github.com>
2024-03-18 16:14:28 +0530

    commiteed

--- settimeoutClosures.js
+++ settimeoutClosures.js
@@ -22,3 +22,13 @@ function x() {
 x();
 
 //the above code will first prints 'namaste js' then after 1 sec  and print i
+// How and y it is printing like this means bcoz, this function   passed to settimeout forms a closure , it remembers a reference to I and forms a closure.
+// Wherever this fucn goes , it  takes the ref of I along with it.
+// Settimeout will takes this callback func and stores it in some place. And attach a timer to it.
+// And js will proceeds with next lines.
+// Once the timer expires (1000 ms completed) then it takes the func and runs  by putting it in callstack.
+// So js doesn’t wait any time .
+
+// Situation :
+// Suppose printing 1,2,3,4,5 after each and every sec.
+// Most developers do it using for loop as below.
