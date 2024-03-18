9cd24dcac9eb5d80d267a86b62afc2baf4397d0a
zeenathkousar <95568153+zeenathkousar@users.noreply.github.com>
2024-03-18 16:03:42 +0530

    commiteed

--- settimeoutClosures.js
+++ settimeoutClosures.js
@@ -10,3 +10,15 @@ x();
 
 //it pritns value of i after one sec.
 //IF WE GIVE  3000 milli secs, it prints after 3secs
+
+function x() {
+  var i = 1;
+  setTimeout(() => {
+    console.log(i);
+  }, 1000);
+  console.log("namaste js");
+}
+
+x();
+
+//the above code will first prints 'namaste js' then after 1 sec  and print i
